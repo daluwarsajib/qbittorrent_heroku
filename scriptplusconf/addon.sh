d6b0624fa9a3b471ab86f66f60f5b2cdf8e85fb4
Daluwar Hossain Sajib <108203575+daluwarsajib@users.noreply.github.com>
2023-02-12 13:19:28 +0600

    Update addon.sh

--- scriptplusconf/addon.sh
+++ scriptplusconf/addon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir --parents /qBittorrent/downloads/
-curl "https://raw.githubusercontent.com/developeranaz/qbittorrent-to-rclone-heroku/main/scriptplusconf/about.html" > /var/www/html/about.html
+curl "https://raw.githubusercontent.com/daluwarsajib/qbittorrent_heroku/main/scriptplusconf/about.html" > /var/www/html/about.html
 curl "https://raw.githubusercontent.com/daluwarsajib/qbittorrent_heroku/main/scriptplusconf/index.html" >/var/www/html/index.html
